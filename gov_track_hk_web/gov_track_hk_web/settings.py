d31e5158b9785dbc86d8e69d02f3b741669093f5
Ho Wa Wong <howa.wong@gmail.com>
2016-08-16 00:25:53 +0800

    settings.py updated

--- gov_track_hk_web/gov_track_hk_web/settings.py
+++ gov_track_hk_web/gov_track_hk_web/settings.py
@@ -129,10 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 
 MORPH_IO_API_KEY = os.environ['MORPH_API_KEY']
