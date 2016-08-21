020d044dec6297b28a907a9f14fcc956635de7fe
Ho Wa Wong <howa.wong@gmail.com>
2016-08-21 06:47:01 +0000

    settings updated

--- gov_track_hk_web/gov_track_hk_web/settings.py
+++ gov_track_hk_web/gov_track_hk_web/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
+STATIC_ROOT = "/srv/web/assets"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
