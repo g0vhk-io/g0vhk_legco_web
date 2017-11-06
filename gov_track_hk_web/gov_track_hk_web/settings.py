710432eeeed02f571a348ee1c907e4e81ef2b7c6
Ho Wa Wong <howa.wong@gmail.com>
2017-11-06 10:03:45 -0500

    Database Argument with Environment Variable and .travis.yml

--- gov_track_hk_web/gov_track_hk_web/settings.py
+++ gov_track_hk_web/gov_track_hk_web/settings.py
@@ -88,7 +88,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': '/etc/mysql/gov_track_hk.cnf',
+            'NAME': os.getenv('MYSQL_DATABASE'),
+            'USER': os.getenv('MYSQL_USER'),
+            'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+            'HOST': os.getenv('MYSQL_HOST'),
+            'PORT': '3306'
         },
     }
 }
